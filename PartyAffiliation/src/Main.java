a8627b37e29b3795140b14e5caf03a44f98d5b15
Curtis-Buswell <buswelca@mail.uc.edu>
2023-06-19 17:35:04 -0400

    Project Complete

--- PartyAffiliation/src/Main.java
+++ PartyAffiliation/src/Main.java
@@ -1,5 +1,36 @@
+//class PartyAnimal
+//   main()
+//        String userParty
+//        output “What is your party affiliation? [ RDI ]”
+//        input userParty
+//        if  userParty == “R” then
+//        output “Your party’s animal is the Elephant”
+//        else if userParty == “D” then
+//        output “Your party’s animal is the Donkey”
+//        else
+//        output “You get the human!”
+//        end if
+//class end
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        String partyAffiliation;
+        System.out.println("What is your party affiliation? [RDI]");
+        if (in.hasNextInt()) {
+            System.out.println("Please try again and input a valid character. [A-Z]");
+            System.exit(0);
+        }
+        partyAffiliation = in.nextLine();
+        if (partyAffiliation.equalsIgnoreCase("r")) {
+            System.out.println("Your party's symbol is the Elephant!");
+        } else if (partyAffiliation.equalsIgnoreCase("d")) {
+            System.out.println("Your party's symbol is the Donkey!");
+        } else if (partyAffiliation.equalsIgnoreCase("i")) {
+            System.out.println("Your party's symbol is the Human!");
+        } else {
+            System.out.println("You get...other?");
+
+        }
     }
 }
