af72849a11026d370a06ed751c99622527739db2
Curtis-Buswell <buswelca@mail.uc.edu>
2023-06-19 23:18:23 -0400

    Project Complete

--- NumCompare/src/Main.java
+++ NumCompare/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner (System.in);
+        int value1;
+        int value2;
+        System.out.print("Please enter the first number: ");
+        if (in.hasNextInt()){}
+        else {
+            System.out.println("Error. Please enter a valid number and try again.");
+            System.exit(0);
+        }
+        value1 = in.nextInt();
+        System.out.print("Please enter the second number: ");
+        if (in.hasNextInt()){}
+        else {
+            System.out.println("Error. Please enter a valid second number and try again.");
+            System.exit(0);
+        }
+        value2 = in.nextInt();
+        if (value1 == value2){
+            System.out.println("The 2 numbers are equal.");
+        } else if (value1 > value2) {
+            System.out.println("The first number is greater than the second number.");
+        }
+        else {
+            System.out.println("The second number is greater than the first number.");
+        }
     }
 }
--- TheaterKiosk/src/Main.java
+++ TheaterKiosk/src/Main.java
@@ -1,5 +1,28 @@
+//class WristBandCheck
+//   main()
+//        num userAge = 0
+//        num DRINKING_AGE = 21
+//        output “What is your age?”
+//        input userAge
+//        if userAge >= DRINKING_AGE then output “Here is your wristband! Be safe.”
+//        end if
+//        return
+//        end class
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int userAge;
+        int DRINKING_AGE = 21;
+        System.out.println("What is your age?");
+        if (in.hasNextInt()){}
+        else {
+            System.out.println("Error. PLease enter a valid age and try again.");
+            System.exit(0);
+        }
+        userAge = in.nextInt();
+        if (userAge >= DRINKING_AGE) {
+            System.out.println("Here is your wristband!");
+        }
     }
 }
